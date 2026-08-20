d5399a0b965fe97e36932874d3cd775373b0c817
Syrenate <lyric.jennings@outlook.com>
2026-08-20 15:36:28 +0100

    Fix testing procedure

--- testing.py
+++ testing.py
@@ -1,7 +1,8 @@
 from test import testSolver
 
 if __name__ == "__main__":
-    user_input = input("What test would you like to conduct? \n\t[A] Solution finding on sample boards.")
-
+    user_input = input("What test would you like to conduct? \n\t[A] Solution finding on sample boards.\n")
+    print("\n\n")
+    
     match user_input.upper():
         case "A": testSolver()
