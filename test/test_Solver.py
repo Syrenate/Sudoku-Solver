d018f5daebc5ede6597e3e5ea87243b35d887189
Syrenate <lyric.jennings@outlook.com>
2026-08-20 12:54:13 +0100

    Timed testing functionality

--- test/test_Solver.py
+++ test/test_Solver.py
@@ -2,20 +2,30 @@ import csv, time
 
 from src.Solver import Board, solveBoard
 
+def timeMethod(base_fn):
+    def timedMethod(name, config):
+        start_time = time.time()
+        base_fn(name, config)
+        print(f"Time taken: {round((time.time() - start_time) * 1000, 2)}ms.\n\n")
+    return timedMethod
+
+
+@timeMethod
+def solveFromConfig(name: str, config: list[str]):
+    board = Board(config)
+
+    print(f"Solving board: {name}")
+    print(board, "\n")
+
+    solution = solveBoard(board)
+    if solution == None:
+        print("No solution found!")
+    else: print(solution)
+
+
 def testSolver(): 
     with open("res/test_puzzles.csv", newline="\n") as file:
         reader = csv.reader(file, delimiter='|')
 
         for file_config in reader: 
-            board = Board(file_config[1].split(","))
-
-            print(f"Solving board: {file_config[0]}")
-            print(board)
-
-            start_time = time.time()
-            solution = solveBoard(board)
-
-            if solution == None:
-                print("No solution found!")
-            else:
-                print(f"Puzzle solved! Time taken: {round((time.time() - start_time) * 1000, 2)}ms. \n{solution}\n")
+            solveFromConfig(file_config[0], file_config[1].split(","))
